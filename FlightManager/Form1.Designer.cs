570f86a084e239fb5b8ade84cf063a08d5c1b68a
mehmedhsyn <78343355+mehmedhsyn@users.noreply.github.com>
2021-11-07 12:41:51 +0200

    Functionality of the main menu

--- FlightManager/Form1.Designer.cs
+++ FlightManager/Form1.Designer.cs
@@ -35,31 +35,36 @@ namespace FlightManager
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(91, 31);
+            this.button1.Location = new System.Drawing.Point(123, 55);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(132, 45);
+            this.button1.Size = new System.Drawing.Size(176, 55);
             this.button1.TabIndex = 0;
-            this.button1.Text = "Admin";
+            this.button1.Text = "Anonym";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(105, 82);
+            this.button2.Location = new System.Drawing.Point(123, 135);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(106, 37);
+            this.button2.Size = new System.Drawing.Size(176, 46);
             this.button2.TabIndex = 1;
             this.button2.Text = "User";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(318, 194);
+            this.ClientSize = new System.Drawing.Size(424, 239);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
 
         }
